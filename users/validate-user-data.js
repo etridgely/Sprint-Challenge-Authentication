bb2d76bdee116565131ba869e21904f7b6277d24
etridgely <5757423+etridgely@users.noreply.github.com>
2020-01-12 14:26:27 -0800

    created user model

---
+++ users/validate-user-data.js
@@ -0,0 +1,13 @@
+function validateUserData(req, res, next) {
+    const userData = req.body;
+    if (!userData.username) {
+        res.status(400).json({ errorMessage: 'missing required username field' });
+    } else if (!userData.password) {
+        res.status(400).json({ errorMessage: 'missing required password field' });
+    } else {
+        console.log('user validated');
+        next();
+    }
+}
+
+module.exports = validateUserData;
